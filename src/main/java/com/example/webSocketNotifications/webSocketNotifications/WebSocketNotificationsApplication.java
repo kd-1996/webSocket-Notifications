7b346b98d093d2205d9467934aeeb473d7c0e422
kailash <kailash.m@beehyv.com>
2023-03-07 19:50:15 +0530

    initial setup

---
+++ src/main/java/com/example/webSocketNotifications/webSocketNotifications/WebSocketNotificationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.webSocketNotifications.webSocketNotifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketNotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketNotificationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webSocketNotifications/webSocketNotifications/WebSocketNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webSocketNotifications.webSocketNotifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketNotificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
