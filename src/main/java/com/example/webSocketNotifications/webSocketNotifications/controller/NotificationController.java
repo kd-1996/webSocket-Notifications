406cbd97b830d179e542efe4bd36543a29d41459
kailash <kailash.m@beehyv.com>
2023-03-07 20:01:30 +0530

    added NotificationController

---
+++ src/main/java/com/example/webSocketNotifications/webSocketNotifications/controller/NotificationController.java
@@ -0,0 +1,7 @@
+package com.example.webSocketNotifications.webSocketNotifications.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class NotificationController {
+}
